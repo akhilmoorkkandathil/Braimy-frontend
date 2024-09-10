55b0f230d7ac5882983266b1582ca9384df27026
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-09-10 17:35:04 +0530

    Deployment

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_BASE_URL: 'http://16.171.148.207:8000',
+    API_BASE_URL: 'http://16.171.160.5:8000',
     RZP_KEY_ID:'rzp_test_pLc9MoYbVxqB7b',
     RZP_KEY_SECRET:'kjN1BI5Ti53kPSRcuoNWwNgY'
   };
