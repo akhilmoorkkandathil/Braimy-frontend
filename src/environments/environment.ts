be422585390b4109a828ca73512b5e973d3d3c18
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-09-12 13:55:28 +0530

    /api for backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
 export const environment = {
     production: false,
-    API_BASE_URL : 'http://localhost:8000/',
+    API_BASE_URL : 'http://localhost:8000/api/',
      API_KEY: "AIzaSyCxpy60nch-d-exY-jX3BNo20qyQRB7lVQ",
      RZP_KEY_ID:'rzp_test_pLc9MoYbVxqB7b',
     RZP_KEY_SECRET:'kjN1BI5Ti53kPSRcuoNWwNgY',
     SOCKET_IO_URL: 'http://localhost:8000/'
 }
-
 
