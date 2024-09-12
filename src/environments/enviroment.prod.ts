f2b3a7c1652d4e216d2b7583cde4ff6bb622eade
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-09-12 11:16:09 +0530

    video call almost done

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,7 +1,8 @@
-export const environment = {
-    production: true,
-    API_BASE_URL: 'http://16.171.160.5:8000',
-    RZP_KEY_ID:'rzp_test_pLc9MoYbVxqB7b',
-    RZP_KEY_SECRET:'kjN1BI5Ti53kPSRcuoNWwNgY'
-  };
-  
+    export const environment = {
+        production: true,
+        API_BASE_URL: 'http://16.171.160.5:8000/',
+        API_KEY: "AIzaSyCxpy60nch-d-exY-jX3BNo20qyQRB7lVQ",
+        RZP_KEY_ID: 'rzp_test_pLc9MoYbVxqB7b',
+        RZP_KEY_SECRET: 'kjN1BI5Ti53kPSRcuoNWwNgY',
+        SOCKET_IO_URL: 'http://16.171.160.5:8000/'
+    };
