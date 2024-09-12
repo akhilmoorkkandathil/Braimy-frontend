d559168b2e5c056a17c8392559b99d3ff68f2fa5
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-09-12 19:24:55 +0530

    https

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,8 +1,8 @@
     export const environment = {
         production: true,
-        API_BASE_URL: ' http://edusolvelearn.site/api/',
+        API_BASE_URL: ' https://edusolvelearn.site/api/',
         API_KEY: "AIzaSyCxpy60nch-d-exY-jX3BNo20qyQRB7lVQ",
         RZP_KEY_ID: 'rzp_test_pLc9MoYbVxqB7b',
         RZP_KEY_SECRET: 'kjN1BI5Ti53kPSRcuoNWwNgY',
-        SOCKET_IO_URL: 'http://edusolvelearn.site/'
+        SOCKET_IO_URL: 'https://edusolvelearn.site/api/'
     };
